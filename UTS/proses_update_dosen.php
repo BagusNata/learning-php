88a2c235bfd001ac62f5846e8d5bb6ae05085d4a
bagusnata330 <bagusnata330@gmail.com>
2022-05-02 12:28:28 +0800

    create update dosen

---
+++ UTS/proses_update_dosen.php
@@ -0,0 +1,32 @@
+<?php
+//Menangkap value dari form
+  $nidn           = $_POST['nidn'];
+  $nama_dosen     = $_POST['namaDosen'];
+  $pendidikan     = $_POST['pendidikan'];
+  $tgl_lahir      = $_POST['tglLahir'];
+  $jenis_kelamin  = $_POST['gender'];
+  $alamat         = $_POST['alamat'];
+  $no_hp          = $_POST['phoneNumber'];
+  $email          = $_POST['email'];
+
+  echo $nidn          . "<br>";
+  echo $nama_dosen    . "<br>";
+  echo $pendidikan    . "<br>";
+  echo $tgl_lahir     . "<br>";
+  echo $jenis_kelamin . "<br>";
+  echo $alamat        . "<br>";
+  echo $no_hp         . "<br>";
+  echo $email         . "<br>";
+
+//Membuat koneksi database
+  include "koneksi.php";
+
+//membuat query insert data
+  $qry = "UPDATE dosen
+          SET nama_dosen = '$nama_dosen', pendidikan = '$pendidikan', 
+              tgl_lahir = '$tgl_lahir', jenis_kelamin = '$jenis_kelamin', 
+              alamat = '$alamat', no_hp = '$no_hp', email = '$email'
+          WHERE nidn = '$nidn' ";
+  $exec = mysqli_query($con, $qry);
+
+  header('location: selectDosen.php?m=1');
--- UTS/updateDosen.php
+++ UTS/updateDosen.php
@@ -25,7 +25,6 @@
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               <a class="dropdown-item" href="insertMhs.php">Data Mahasiswa</a>
               <a class="dropdown-item" href="insertJurusan.php">Data Jurusan</a>
-              <a class="dropdown-item" href="insertDosen.php">Data Dosen</a>
             </div>
           <!-- Navbar Select -->
           <li class="nav-item dropdown active">
@@ -35,7 +34,6 @@
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               <a class="dropdown-item" href="selectMhs.php">Data Mahasiswa</a>
               <a class="dropdown-item" href="selectJurusan.php">Data Jurusan</a>
-              <a class="dropdown-item" href="selectDosen.php">Data Dosen</a>
             </div>
           </li>
           <!-- Navbar Update -->
@@ -46,7 +44,6 @@
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               <a class="dropdown-item" href="updateMhs.php">Data Mahasiswa</a>
               <a class="dropdown-item" href="updateJurusan.php">Data Jurusan</a>
-              <a class="dropdown-item" href="updateDosen.php">Data dosen</a>
             </div>
           </li>
           <!-- Navbar Delete -->
@@ -57,7 +54,6 @@
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
               <a class="dropdown-item" href="deleteMhs.php">Data Mahasiswa</a>
               <a class="dropdown-item" href="deleteJurusan.php">Data Jurusan</a>
-              <a class="dropdown-item" href="deleteDosen.php">Data dosen</a>
             </div>
           </li>
         </ul>
@@ -67,60 +63,46 @@
     <!-- TABLE -->
     <div class="container"> 
       </br> </br>
-      <h1 class="title">UJIAN TENGAH SEMESTER</h1>
+      <h1 class="title">Update Data Dosen</h1>
       </br> 
-      <div class="table-responsive-sm" 
+      <div class="table-responsive-sm"  
           style="box-shadow: -10px 0px 13px -7px #000000, 10px 0px 13px -7px #000000,
           5px 17px 41px 27px rgb(0 0 0 / 46%);">
         <table class="table" border="3">
           <thead class="thead-dark table_title">
             <tr>
-              <th scope="col">NIM</th>
-              <th scope="col">Nama Mahasiwa</th>
-              <th scope="col">Kode Jurusan</th>
-              <th scope="col">Nama Jurusan</th>
-              <th scope="col">Gender</th>
+              <th scope="col">NIDN</th>
+              <th scope="col">Nama Dosen</th>
+              <th scope="col">Pendidikan</th>
+              <th scope="col">Tanggal Lahir</th>
+              <th scope="col">Jenis Kelamin</th>
               <th scope="col">Alamat</th>
               <th scope="col">No. Hp</th>
               <th scope="col">Email</th>
-              <th scope="col">NIDN</th>
-              <th scope="col">Dosen Wali</th>
-              <th scope="col" colspan="2">Action</th>
+              <th scope="col">Action</th>
             </tr>
           </thead>
 
           <?php
             include "koneksi.php";
 
-            $qry = "SELECT 
-                        mahasiswa.*, jurusan.nama_jurusan, dosen.nama_dosen
-                    FROM
-                        jurusan, mahasiswa , dosen
-                    WHERE
-                        mahasiswa.kode_jurusan = jurusan.kode_jurusan AND
-                        mahasiswa.nidn = dosen.nidn
-                    ORDER BY mahasiswa.nim";
+            $qry = "SELECT * FROM dosen";
             $exec = mysqli_query($con, $qry);
             while($data = mysqli_fetch_array($exec)) {
           ?>
 
-          <tbody class="table-body">
+          <tbody class="table-bodyMhs">
             <tr>
-              <td style='text-align:center;'> <?php echo $data['nim']            ?> </td>
-              <td style='text-align:center;'> <?php echo $data['nama_mhs']       ?> </td>
-              <td style='text-align:center;'> <?php echo $data['kode_jurusan']   ?> </td>
-              <td style='text-align:center;'> <?php echo $data['nama_jurusan']   ?> </td>
-              <td style='text-align:center;'> <?php echo $data['jenis_kelamin']  ?> </td>
-              <td style='text-align:center;'> <?php echo $data['alamat']         ?> </td>
-              <td style='text-align:center;'> <?php echo $data['no_hp']          ?> </td>
-              <td style='text-align:center;'> <?php echo $data['email']          ?> </td>
-              <td style='text-align:center;'> <?php echo $data['nidn']     ?> </td>
-              <td style='text-align:center;'> <?php echo $data['nama_dosen']     ?> </td>
+              <td> <?php echo $data['nidn']           ?> </td>
+              <td> <?php echo $data['nama_dosen']     ?> </td>
+              <td> <?php echo $data['pendidikan']     ?> </td>
+              <td> <?php echo $data['tgl_lahir']      ?> </td>
+              <td> <?php echo $data['jenis_kelamin']  ?> </td>
+              <td> <?php echo $data['alamat']         ?> </td>
+              <td> <?php echo $data['no_hp']          ?> </td>
+              <td> <?php echo $data['email']          ?> </td>
               <td style="text-align: center;"> 
-                <a style="color:black;" href="editMhs.php?nim=<?php echo $data['nim']?>" class="btn-edit"> <img src="Assets/Image/b_edit.png"> Edit </a>
-              </td>
-              <td style="text-align: center;"> 
-                <a style="color:black;" href="proses_delete_mhs.php?nim=<?php echo $data['nim']?>" class="btn-del"> <img src="Assets/Image/b_drop.png"> Delete </a>
+                <a style="color:black;" href="editDosen.php?nidn=<?php echo $data['nidn']?>" class="btn-edit"> <img src="Assets/Image/b_edit.png"> Edit </a>
               </td>
             </tr>
             <?php } ?>
@@ -135,8 +117,7 @@
       <div class="flash-data" data-flashdata="<?= $_GET['m']; ?>"></div>
     <?php endif; ?>
 
-  
-    <!-- Optional JavaScript --> 
+    <!-- Optional JavaScript -->
     <!-- SweetAlert2 --> 
     <script src="jquery-3.6.0.min.js"></script>
     <script src="sweetalert2.all.min.js"></script>
@@ -158,35 +139,7 @@
                   }
           })
       })
-      
-      $('.btn-del').on('click', function(e) {
-          e.preventDefault();
-          const href = $(this).attr('href')
-          Swal.fire({
-              title : "Are You Sure?",
-              text  : 'Record will be deleted?',
-              icon  : 'warning',
-              showCancelButton  : true,
-              confirmButtonColor: '#3085d6',
-              cancelButtonColor : '#d33',
-              confirmButtonText : 'Delete Record',
-          }). then ((result) => {
-                  if (result.value) {
-                      document.location.href = href;
-                  }
-          })
-      })
-
-      const flashdata = $('.flash-data').data('flashdata')
-      if (flashdata) {
-          Swal.fire({
-              icon  : 'success',
-              title : 'Success',
-              text  : 'Record has been deleted!',
-          })
-      }
-    </script>
-
+      </script>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
