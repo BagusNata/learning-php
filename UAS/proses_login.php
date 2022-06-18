7bc83c176aee62fa9aa03ae984045b28e03dab5d
bagusnata330 <bagusnata330@gmail.com>
2022-06-18 21:36:52 +0800

    Create process system by applying the OOP concept

--- UAS/proses_login.php
+++ UAS/proses_login.php
@@ -1,36 +1,21 @@
-<?php 
-//koneksikan ke database
-  include "koneksi.php";
+<?php
+  include "proses_class_system.php";
+  $clogin = new login();
 
-  class Login extends database{
-    function __construct(){
-    parent::__construct();
-    }
-    
-    function login_proses($username, $password)
-    {
-      //mencocokkan data user(username,password) pada database
-        $qry = "SELECT * FROM user 
-        WHERE (username = '$username' OR email = '$username') AND password = '$password'";
-        $exec = mysqli_query($this->conn,$qry);
-        $data = mysqli_fetch_array($exec);
-        
-        return $data;
-    }
-  }
-
-  $clogin = new Login();
-  $username = $_POST['username'];
-  $password = $_POST['password'];
-  $data = $clogin->login_proses($username, $password);
-
-  if($data)
-  {
+	if (isset($_POST['submitLogin'])){
+		$data = array(
+			'username'  => $_POST['username'],
+			'password'  => $_POST['password'],
+		);
+		
+		$exec = $clogin->login_user($data);
+		if($exec){
+      //Memulai session
       session_start();
       $_SESSION['username'] = $data['username'];
-      echo "<script>alert('Login Berhasil');window.location = 'index.php'</script>";
-      
-  }else{
-      echo "Login gagal, user tidak ditemukan";
-  }
+			header('location: index.php');
+		}else{
+			echo "Login gagal, user tidak ditemukan";;
+		}
+	}
 ?>
